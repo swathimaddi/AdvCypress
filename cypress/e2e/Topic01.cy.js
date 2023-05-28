47b7764c005f44988413b94654dae33f6536cd7d
swathimaddi <swathimaddi03@gmail.com>
2023-05-28 17:58:58 +0530

    commit2

--- cypress/e2e/Topic01.cy.js
+++ cypress/e2e/Topic01.cy.js
@@ -9,7 +9,7 @@ describe('Topic01_Suite',() => {
       cy.visit('https://www.icicibank.com')
       cy.get("#new-header > div > div:nth-child(1) > div > div > nav.left-side-menu.menu-level-1-child > ul").should("be.visible")
       cy.wait(3000)
-     // cy.viewport(500,800) -- desktop
+     cy.viewport(500,800) //-- desktop
     // cy.viewport("iphone-x") //-- mobile
       cy.wait(3000)
       cy.get("#new-header > div > div:nth-child(1) > div > div > nav.left-side-menu.menu-level-1-child > ul").should("not.be.visible")
